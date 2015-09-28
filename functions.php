7d73444b465c8a2f6a5d10d12610d1e4aa5ee9f5
Pontus <pontussunden@pontussunden.gu.gu.se>
2015-09-28 11:04:49 +0200

    Site width, header image, title tag

--- functions.php
+++ functions.php
@@ -100,6 +100,20 @@ function psu_do_default_sidebar() {
 }
 
 
+//* Modify itle tag
+
+remove_filter( 'wp_title', 'genesis_default_title', 10, 3 ); //Default title
+//remove_action( 'genesis_site_title', 'genesis_seo_site_title' ); //Genesis Extra Stuff
+//remove_action( 'genesis_site_description', 'genesis_seo_site_description' ); //Genesis Extra Stuff
+add_filter( 'wp_title', 'psu_title_tag', 10, 3 );
+function psu_title_tag($title) {
+	$standard = get_bloginfo('name') .', '. __( "Sahlgrenska Academy's news site", 'magazine' );
+	if ( is_front_page() )
+		return  $standard;
+	else
+		return get_the_title() .' - '. $standard;
+}
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 //* HELPER FUNCTIONS
